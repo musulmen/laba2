447c65c1fc35f0e98f1b2e73cc098ce5d4393a0d
musulmen <vopot225@gmail.com>
2024-11-10 16:47:53 +0500

    Лабораторная работа №2

--- homework5/1.py
+++ homework5/1.py
@@ -1,17 +1,9 @@
-def my_generate(start=0, stop=30, step=1):
-    number = start
-    while number <= stop:
-        yield number
-        number += step
-
-def get_odd_numbers():
-    for num in my_generate(1, 29, 2):
+def get_number():
+    for num in range(1, 29, 2):
         yield num
 
-ranger = get_odd_numbers()
+n = list(get_number())
 
-numbers = list(ranger)
-
-print("Первое значение:", numbers[0])
-print("Пятое значение:", numbers[4])
-print("Последнее значение:", numbers[-1])
+print("Первое значение:", n[0])
+print("Пятое значение:", n[4])
+print("Последнее значение:", n[-1])
